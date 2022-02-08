7ff54573550ac37b0c4cadaa2610d23eeb4d77bb
tae100k <tkim3@oswego.edu>
2022-02-08 20:33:33 +0900

    modify: 이미지 경로 수정 [#6]

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import * as S from './styles';
-// import handleClick from './function';
 
 const LOGIN_STATUS = '로그아웃';
+const mainIcon = require('assets/images/icon.png');
+const producerIcon = require('assets/images/producer-icon.png');
+const eclipseIcon = require('assets/images/eclipseIcon.png');
+const menuIcon = require('assets/images/whiteMenuIcon.png');
 
 const Header = (): JSX.Element => {
   const [isClicked, setIsClicked] = useState(false);
@@ -19,12 +22,9 @@ const Header = (): JSX.Element => {
           <S.StyledEclipse />
         </S.StyledEclipseWrapper>
         <S.StyledInnerDiv>
-          <S.StyledIcon src="https://i.ibb.co/M85H48c/icon.png" alt="icon" />
+          <S.StyledIcon src={mainIcon} alt="icon" />
           <S.StyledInfoDiv>
-            <S.StyledInfoIcon
-              src="https://i.ibb.co/8YzKN3t/producer-icon.png"
-              alt="infoIcon"
-            />
+            <S.StyledInfoIcon src={producerIcon} alt="infoIcon" />
             <S.StyledInfoInnerDiv>A가공업체</S.StyledInfoInnerDiv>
             <S.StyledInfoLogin>{LOGIN_STATUS}</S.StyledInfoLogin>
           </S.StyledInfoDiv>
@@ -40,17 +40,11 @@ const Header = (): JSX.Element => {
             event.stopPropagation();
           }}>
           <S.StyledWhiteBgHeader>
-            <S.StyledEclipseIcon
-              src="https://i.ibb.co/fXZSLq9/eclipse-Icon.png"
-              alt="eclipse-Icon"
-            />
+            <S.StyledEclipseIcon src={eclipseIcon} alt="eclipse-Icon" />
           </S.StyledWhiteBgHeader>
           <S.StyledWhiteBgBodyContainer>
             <S.StyledWhiteBgBodyIconWrapper>
-              <S.StyledInfoIconWhite
-                src="https://i.ibb.co/cv4pzQL/white-Menu-Icon.png"
-                alt="white-Menu-Icon"
-              />
+              <S.StyledInfoIconWhite src={menuIcon} alt="white-Menu-Icon" />
               <S.StyledWhiteBgBody>파트너정밀가공</S.StyledWhiteBgBody>
             </S.StyledWhiteBgBodyIconWrapper>
             <S.StyledWhiteBgBody>{LOGIN_STATUS}</S.StyledWhiteBgBody>
