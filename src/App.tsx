d29338c73e51b200af963cdf3e1931dddb89440c
tae100k <tkim3@oswego.edu>
2022-02-08 20:38:54 +0900

    modify: 충돌 부분 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import Header from 'components/index';
 import React from 'react';
 
 const App = (): JSX.Element => {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
+  return <div>app;</div>;
 };
 
 export default App;
